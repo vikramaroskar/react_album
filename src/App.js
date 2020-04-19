c9d25be164367008bae75f5abf5ea937b565b0cd
vikram <vikram.aroskar@gmail.com>
2020-04-19 13:23:06 +0530

    single photo, tailwind post css, main css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-full" src="https://source.unsplash.com/random" alt="random"/>
+
+        <div className="px-6 py-4">
+          <div className="font-bold text-purple-500 text-xl">
+            Photo by unsplash
+          </div>
+          <ul>
+            <li>
+              <strong> Views: </strong>
+              4000
+            </li>
+            <li>
+              <strong> Downloads: </strong>
+              200
+            </li>
+            <li>
+              <strong> Likes: </strong>
+              400
+            </li>
+          </ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #tag1 
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #tag2
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #tag3
+          </span>
+        </div>
       </div>
     );
   }
